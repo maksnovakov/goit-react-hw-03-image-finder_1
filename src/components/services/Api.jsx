dcfd53006b3ee2579b1f1becb7f8261c033825e8
Maks Novakov <maksnovakov78@gmail.com>
2022-11-10 12:48:49 +0200

    1

---
+++ src/components/services/Api.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const http = {
+    get(url) {
+        return axios.get(url);
+    },
+    post(url, body) {
+        return axios.post(url, body);
+    },
+};
+
+
